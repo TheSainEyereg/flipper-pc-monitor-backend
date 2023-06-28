4305b5d122dc320c2b12aac963d013ab6970fde8
voidptr_t <39582793+Sanceilaks@users.noreply.github.com>
2023-06-28 17:24:09 +0300

    work pls

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,7 @@ async fn data_sender(flipper: Peripheral) {
     println!("Writing {:?} to Flipper", systeminfo.as_bytes());
 
     let mut systeminfo_bytes = systeminfo.as_bytes().to_vec();
-    systeminfo_bytes.push(b'\0');
+    systeminfo_bytes.extend_from_slice(&[0x00, 0xF0, 0xAA]);
 
     flipper.write(cmd_char, &systeminfo_bytes, 
         btleplug::api::WriteType::WithoutResponse).await.expect("Failed to write to Flipper");
