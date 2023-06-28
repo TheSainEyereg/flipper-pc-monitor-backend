5c9f22dcfbbac6bfb02a66daf90878dd2044acd1
voidptr_t <39582793+Sanceilaks@users.noreply.github.com>
2023-06-28 15:41:29 +0300

    refactoring 2x

---
+++ src/flipper_manager.rs
@@ -0,0 +1,35 @@
+use uuid::Uuid;
+use btleplug::api::{
+    Central, CentralEvent, Manager as _, Peripheral as _, ScanFilter,
+};
+use btleplug::platform::{Adapter, Manager, Peripheral, PeripheralId};
+use futures::stream::StreamExt;
+
+// TODO: Use this or remove (-_-)
+pub const FLIPPER_CHARACTERISTIC_UUID: Uuid = Uuid::from_u128(0x19ed82ae_ed21_4c9d_4145_228e62fe0000);
+
+pub async fn get_central(manager: &Manager) -> Adapter {
+    manager
+        .adapters()
+        .await
+        .unwrap()
+        .into_iter()
+        .nth(0)
+        .unwrap()
+}
+
+pub async fn get_flipper(central: &Adapter, id: &PeripheralId) -> Option<Peripheral> {
+    for p in central.peripherals().await.unwrap().iter().filter(|p| p.id() == *id) {
+        if p.properties()
+            .await
+            .unwrap()
+            .unwrap()
+            .local_name
+            .iter()
+            .any(|name| name.contains("Flipper"))
+        {
+            return Some(p.clone());
+        }
+    }
+    None
+}
--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,14 @@
 #![feature(if_let_guard)]
 
+use std::error::Error;
 use btleplug::api::{
     Central, CentralEvent, Manager as _, Peripheral as _, ScanFilter,
 };
-use btleplug::platform::{Adapter, Manager, Peripheral, PeripheralId};
+use btleplug::platform::{Manager, Peripheral};
 use futures::stream::StreamExt;
-use std::error::Error;
-use uuid::Uuid;
 
-// TODO: Use this or remove (-_-)
-const FLIPPER_CHARACTERISTIC_UUID: Uuid = Uuid::from_u128(0x19ed82ae_ed21_4c9d_4145_228e62fe0000);
 
-async fn get_central(manager: &Manager) -> Adapter {
-    manager
-        .adapters()
-        .await
-        .unwrap()
-        .into_iter()
-        .nth(0)
-        .unwrap()
-}
-
-async fn get_flipper(central: &Adapter, id: &PeripheralId) -> Option<Peripheral> {
-    for p in central.peripherals().await.unwrap().iter().filter(|p| p.id() == *id) {
-        if p.properties()
-            .await
-            .unwrap()
-            .unwrap()
-            .local_name
-            .iter()
-            .any(|name| name.contains("Flipper"))
-        {
-            return Some(p.clone());
-        }
-    }
-    None
-}
+mod flipper_manager;
 
 async fn data_sender(flipper: Peripheral) {
     
@@ -45,7 +18,7 @@ async fn data_sender(flipper: Peripheral) {
 async fn main() -> Result<(), Box<dyn Error>> {
     let manager = Manager::new().await?;
 
-    let central = get_central(&manager).await;
+    let central = flipper_manager::get_central(&manager).await;
 
     let mut events = central.events().await?;
 
@@ -54,12 +27,12 @@ async fn main() -> Result<(), Box<dyn Error>> {
     while let Some(event) = events.next().await {
         match event {
             CentralEvent::DeviceDiscovered(id) => {
-                if let Some(flp) = get_flipper(&central, &id).await {
+                if let Some(flp) = flipper_manager::get_flipper(&central, &id).await {
                     flp.connect().await.expect(format!("Failed to connect to Flipper {}", id.to_string()).as_str());
                 }
             }
             CentralEvent::DeviceConnected(id) => {
-                if let Some(flp) = get_flipper(&central, &id).await {
+                if let Some(flp) = flipper_manager::get_flipper(&central, &id).await {
                     flp.discover_services().await?;
                     tokio::spawn(data_sender(flp));
                 };
