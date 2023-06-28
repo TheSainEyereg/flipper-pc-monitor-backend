74ccdd921c64bed1d41e9ae6f502ece660878623
voidptr_t <39582793+Sanceilaks@users.noreply.github.com>
2023-06-28 16:52:16 +0300

    now data sends to flipper

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 
 use std::error::Error;
 use btleplug::api::{
-    Central, CentralEvent, Manager as _, Peripheral as _, ScanFilter,
+    Central, CentralEvent, Manager as _, Peripheral as _, ScanFilter, Characteristic,
 };
 use btleplug::platform::{Manager, Peripheral};
 use futures::stream::StreamExt;
@@ -13,7 +13,12 @@ mod system_info;
 async fn data_sender(flipper: Peripheral) {
     // TODO: AMD (suck) support
     let systeminfo = serde_json::to_string(&system_info::SystemInfo::get_system_info().await).unwrap();
-
+    let chars = flipper.characteristics();
+    let cmd_char = chars
+        .iter()
+        .find(|c| c.uuid == flipper_manager::FLIPPER_CHARACTERISTIC_UUID)
+        .expect("Flipper Characteristic not found");
+    flipper.write(cmd_char, systeminfo.as_bytes(), btleplug::api::WriteType::WithoutResponse).await.expect("Failed to write to Flipper");
 }
 
 #[tokio::main]
