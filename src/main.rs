fffded268050358ce77c56d316c6fafd1fd65a0b
voidptr_t <39582793+Sanceilaks@users.noreply.github.com>
2023-06-28 17:05:32 +0300

    logs

--- src/main.rs
+++ src/main.rs
@@ -18,29 +18,39 @@ async fn data_sender(flipper: Peripheral) {
         .iter()
         .find(|c| c.uuid == flipper_manager::FLIPPER_CHARACTERISTIC_UUID)
         .expect("Flipper Characteristic not found");
+    println!("Writing {:?} to Flipper", systeminfo.as_bytes());
     flipper.write(cmd_char, systeminfo.as_bytes(), btleplug::api::WriteType::WithoutResponse).await.expect("Failed to write to Flipper");
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
+    pretty_env_logger::init();
+
     let manager = Manager::new().await?;
 
     let central = flipper_manager::get_central(&manager).await;
+    println!("Found {:?} adapter", central.adapter_info().await.unwrap());
 
     let mut events = central.events().await?;
 
+    println!("Scanning...");
     central.start_scan(ScanFilter::default()).await?;
 
     while let Some(event) = events.next().await {
         match event {
             CentralEvent::DeviceDiscovered(id) => {
+                println!("Device Discovered: {}", &id.to_string());
                 if let Some(flp) = flipper_manager::get_flipper(&central, &id).await {
+                    println!("Connecting to Flipper {}", &id.to_string());
                     flp.connect().await.expect(format!("Failed to connect to Flipper {}", id.to_string()).as_str());
                 }
             }
             CentralEvent::DeviceConnected(id) => {
                 if let Some(flp) = flipper_manager::get_flipper(&central, &id).await {
+                    println!("Connected to Flipper {}\nDiscover Services", &id.to_string());
                     flp.discover_services().await?;
+                    println!("Services Discovered");
+
                     tokio::spawn(data_sender(flp));
                 };
             }
